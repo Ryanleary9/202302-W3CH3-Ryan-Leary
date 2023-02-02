71e96f1473728fc67b31dcfa65d2ec75390e61ff
Ryan <ryanlearyzafra@gmail.com>
2023-02-02 01:03:48 +0100

    ✨ Add mockpets

---
+++ src/mocks/mockpets.ts
@@ -0,0 +1,8 @@
+import { petList } from '../components/models/pet';
+
+export const PET: petList[] = [
+  new petList('Chop', 'Labrador', 'Ryan'),
+  new petList('Candy', 'Mastin', 'Ryan'),
+  new petList('Belford', 'Black Bear', 'Ryan'),
+  new petList('Juanjo', 'Chimpance', 'Ryan'),
+];
